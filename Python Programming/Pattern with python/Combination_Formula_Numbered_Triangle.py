5d8cc82a84003f21ab325cac34930e422008ab2f
vijayshiv <vijay.aryaa1213@gmail.com>
2022-09-14 01:59:51 +0530

    Edited

--- Python Programming/Pattern with python/Combination_Formula_Numbered_Triangle.py
+++ Python Programming/Pattern with python/Combination_Formula_Numbered_Triangle.py
@@ -1,9 +1,9 @@
 n = int(input("Enter the length : "))
 
-for i in range(1, n + 1):
+for i in range(0, n + 1):
     icj = 1
-    for j in range(1, i + 1):
-        print(icj, end=" ")
-        icjp1 = (icj * (i - j)) // (j + 1)
+    for j in range(0, i + 1):
+        print(f'{icj}  ', end=" ")
+        icjp1 = icj * (i - j) // (j + 1)
         icj = icjp1
     print()
