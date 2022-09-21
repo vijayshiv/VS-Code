d5ed014a84c1312ef693fcbd31244db2dc183f55
vijayshiv <vijay.aryaa1213@gmail.com>
2022-09-22 02:29:07 +0530

    Edited

---
+++ Java Programming/Arrays/2DArray/SearchSortedTwoDArray.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class SearchSortedTwoDArray{
+    public static void main(String[] args)throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the length of square matrix -> ");
+        int n = in.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.println("The matrix is here ->");
+        display(arr);
+
+
+        System.out.print("Enter the value you want to find -> ");
+        int x = in.nextInt();
+        int i = 0;
+        int j = arr[0].length - 1;
+
+        while(i < arr.length && j >= 0){
+            if(x == arr[i][j]){
+                System.out.println( "The cooridates are -> (" + (i+1) +", " + (j+1) + ")");
+                return;
+            } else if(x < arr[i][j]){
+                j--;
+            } else{
+                i++;
+            }
+        }
+        
+        System.out.println("Not Found");
+    
+    }
+
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
