b6cc89a4fff48916ecddbc12eb71c35c3305c9d4
vijayshiv <vijay.aryaa1213@gmail.com>
2022-09-29 23:00:45 +0530

    Edited

---
+++ Competetive/LeetCode Questions/FourOneZero.java
@@ -0,0 +1,84 @@
+/*
+410. Split Array Largest Sum
+
+Given an array arr which consists of non-negative integers and an integer m, 
+you can split the array into m non-empty continuous subarrays.
+Write an algorithm to minimize the largest sum among these m subarrays.
+
+Input: nums = [7,2,5,10,8], m = 2
+Output: 18
+Explanation:
+There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8],
+where the largest sum among the two subarrays is only 18.
+Example 2:
+
+Input: nums = [1,2,3,4,5], m = 2
+Output: 9
+Example 3:
+
+Input: nums = [1,4,4], m = 3
+Output: 4
+
+ */
+/* https://leetcode.com/problems/split-array-largest-sum/  */
+
+
+import java.util.*;
+
+public class FourOneZero{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the length of the array -> ");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements in the array -> ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        System.out.print("Enter the number of peices you want to divide the array -> ");
+        int m = in.nextInt();
+
+        int ans = splitArray(arr, m);
+        System.out.println("The minimum of max value is -> " + ans);
+
+        in.close();
+    }
+    public static int splitArray(int[] arr, int m) {
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            start = Math.max(start, arr[i]); // in the end of the loop this will contain the max item of the array
+            end += arr[i];
+        }
+
+        // binary search
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : arr) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > m) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        return end; // here start == end
+    }
+}
---
+++ Competetive/LeetCode Questions/RotationCount.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class RotationCount {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the length of the array -> ");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements in the array -> ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        int ans = countRotaions(arr);
+        System.out.println("The number of rotations are : " + ans);
+        in.close();
+    }   
+    public static int countRotaions(int[] arr){
+        int pivot = findPivot(arr);
+        return pivot + 1;
+    }     
+    public static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            // the 4 cases are over here
+
+            if(mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid - 1;
+            }
+            if(arr[mid] <= arr[start]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+    public static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
