785259ef71a8019eb284488b6d92dedd34c8a511
vijayshiv <vijay.aryaa1213@gmail.com>
2022-10-03 23:24:40 +0530

    Edited

--- Kunal Kushwaha/Binary Search/BinarySearch.java
+++ Kunal Kushwaha/Binary Search/BinarySearch.java
@@ -25,7 +25,7 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
         
-
+        
         while(start <= end){
             int mid = (start + end) / 2;
 
