c02bf5ca3bbc5b359c1a60ecd0bd013f90bb46d0
vijayshiv <vijay.aryaa1213@gmail.com>
2022-11-09 11:55:13 +0530

    Edited

---
+++ Java Programming/Recursion and Backtracking/KeypadCombination.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class KeypadCombination {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number code = ");
+        String str = in.next();
+        ArrayList<String> words = getKPC(str);
+        System.out.println("The combination of words are " + words); 
+        in.close();
+    }
+    static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+    public static ArrayList<String> getKPC(String str){
+        if(str.length() == 0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+
+        ArrayList<String> rres = getKPC(ros);
+        ArrayList<String> mres = new ArrayList<>();
+
+        String codeforch = codes[ch - '0'];
+        for(int i = 0; i < codeforch.length(); i++){
+            char chcode = codeforch.charAt(i);
+            for (String rstr : rres) {
+                mres.add(chcode + rstr);
+            }
+        }
+        return mres;
+    }
+}
---
+++ Java Programming/Recursion and Backtracking/SubSequenceReccursion.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class SubSequenceReccursion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the String -> ");
+        String str = in.next();
+        ArrayList<String> res = gss(str);
+        System.out.println("The Resulting substrings are " + res);
+        in.close();
+
+    }
+    public static ArrayList<String> gss(String str){
+        if(str.length() == 0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        ArrayList<String> rres = gss(ros);
+
+        ArrayList<String> mres = new ArrayList<>();
+
+        for (String rstr : rres) {
+            mres.add("_" + rstr);
+        }
+        for (String rstr : rres) {
+            mres.add(ch + rstr);
+        }
+        return mres;
+    }
+}
