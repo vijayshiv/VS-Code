e55ab18e468ea10e72d0d6e202152e1c866d1330
vijayshiv <vijay.aryaa1213@gmail.com>
2023-05-20 01:12:09 +0530

    Edited

--- C Tutorial/miscellenous_file.c
+++ C Tutorial/miscellenous_file.c
@@ -46,6 +46,8 @@ void main()
     } while (i <= 5);
 }
 */
+
+/*
 void main()
 {
     printf("while \n");
@@ -56,6 +58,7 @@ void main()
         i++;
     }
 }
+*/
 
 
 /*
@@ -67,5 +70,45 @@ void main()
     {
         printf("vj \n");
     }
+}
+*/
+
+/*
+void main(){
+    int n,k;
+    int fun(int);
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    k = fun(n);
+    printf("The sum of n terms is : %d", k);
+}
+int fun(int a){
+    int s;
+    if(a == 1){
+        return a;
+    }
+    s = a + fun(a-1);
+    return s;
+}
+*/
+
+/*
+void main()
+{
+    int arr[3] = {4, 5, 6};
+    int a = 2;
+    a = 22;
+    a = 32;
+    a = 9;
+    float f = 2.9456;
+    printf("%u\n", a); // 4294967295 this is 2^32 - 1
+    printf("%f\n", f);
+    printf("%d\n", sizeof(8.45f));
+    printf("%d\n", &a);        // 6422300 this is address of a
+    printf("%d\a", sizeof(a)); // 32-bit integer size 4
+    printf("%f", f + a);
+
+    int num = 99;
+    printf("%d %o %x %p %u %ld %i", num, num, num, num, num, num, num);
 }
 */
