23c8792d9cfc8a610180c9d092409e915e545aba
vijayshiv <vijay.aryaa1213@gmail.com>
2023-05-20 01:15:42 +0530

    Edited

--- Java Programming/Dynamic Programming/ClimbingStairs.java
+++ Java Programming/Dynamic Programming/ClimbingStairs.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class ClimbingStairs {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        in.close();
     }
 }
