1565ef73201e52bb1cda8d36e0f31546ee2a66d0
vijayshiv <vijay.aryaa1213@gmail.com>
2023-06-04 00:34:49 +0530

    edited

--- C Tutorial/preprocessor_inclusion_example.c
+++ C Tutorial/preprocessor_inclusion_example.c
@@ -1,2 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "C:\Users\ACER\Desktop\VS code\C Tutorial\preprocessor_inclusion_file.c"
+void main()
+{
+    int a, b;
+    printf("Enter the two number : ");
+    scanf("%d %d", &a, &b);
+    int ans = sum(a, b);
+    printf("The ans in file 2 %d", ans);
+}
--- C Tutorial/preprocessor_inclusion_file.c
+++ C Tutorial/preprocessor_inclusion_file.c
@@ -1,14 +1,9 @@
-#include<stdio.h>
-int sum(int , int);
-int a,b;
-int s;
-void main()
+#include <stdio.h>
+
+int sum(int x, int y)
 {
-    scanf("%d %d",&a,&b);
-    int ans = sum(a,b);
-    printf("sum = %d", ans);
-}
-int sum(int x, int y){
+    int s;
+    printf("The sum in file 1 : \n");
     s = x + y;
     return s;
 }
