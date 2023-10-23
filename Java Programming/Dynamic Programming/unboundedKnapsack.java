0ef4e7144200ca46b8bc7491a9fb71d4943c2838
vijayshiv <vijay.aryaa1213@gmail.com>
2023-10-23 13:10:02 +0530

    New Added

--- Java Programming/Dynamic Programming/unboundedKnapsack.java
+++ Java Programming/Dynamic Programming/unboundedKnapsack.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class unboundedKnapsack {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int arr = in.nextInt();
     }
 }
